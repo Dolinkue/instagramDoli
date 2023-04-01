5284bed356aedc3a2de09d97be4d02eb63c5cf1d
Nicolas Dolinkue <dolinkue_n@hotmail.com>
2023-04-01 18:30:03 -0300

    more profile

---
+++ instagramDoli/Model/Models.swift
@@ -0,0 +1,63 @@
+//
+//  Models.swift
+//  instagramDoli
+//
+//  Created by Nicolas Dolinkue on 01/04/2023.
+//
+
+import Foundation
+
+public enum UserPostType {
+    case photo, video
+}
+
+public struct UserPost {
+    let indentifier: String
+    let postType: UserPostType
+    let thumbnailImage: URL
+    let postURL: URL
+    let caption: String?
+    let likeCount: [PostLike]
+    let comments: [PostComment]
+    let createDate: Date
+    let taggedUsers: [String]
+}
+
+public struct PostComment {
+    let identifier: String
+    let username: String
+    let text: String
+    let createdDate: Date
+    let likes: [CommentLike]
+}
+
+public struct PostLike {
+    let username: String
+    let postIdentifier: String
+}
+
+public struct CommentLike {
+    let username: String
+    let commentIdentifier: String
+}
+
+public struct User {
+    let username: String
+    let bio: String
+    let name: (first: String, last: String)
+    let birthDate: Date
+    let gender: Gender
+    let counts: UserCount
+    let joinDate: Date
+}
+
+public struct UserCount {
+    let followers: Int
+    let followings: Int
+    let post: Int
+}
+
+public enum Gender {
+    case male
+    case female, other
+}
--- instagramDoli/Views/Cells/PhotoCollectionViewCell.swift
+++ instagramDoli/Views/Cells/PhotoCollectionViewCell.swift
@@ -6,9 +6,48 @@
 //
 
 import UIKit
+import SDWebImage
 
 class PhotoCollectionViewCell: UICollectionViewCell {
     static let identifier = "PhotoCollectionViewCell"
+    
+    private let photoImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.clipsToBounds = true
+        imageview.contentMode = .scaleAspectFill
+        return imageview
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        photoImageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubview(photoImageView)
+        contentView.clipsToBounds = true
+        accessibilityLabel = "User post image"
+        accessibilityHint = "Double-tap to open post"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public func configure(with model: UserPost) {
+        let url = model.thumbnailImage
+        photoImageView.sd_setImage(with: url)
+    }
+    
+    public func configureImage(with imageName: String) {
+        photoImageView.image = UIImage(named: imageName)
+    }
     
     
 }
